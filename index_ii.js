16df4575ee0f7495e1233aa8bb7adeaffa52e23f
yjb23tech <yjb23tech@gmail.com>
2024-02-19 21:30:55 +0000

    We got there lol

--- index_ii.js
+++ index_ii.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 
@@ -35,17 +36,69 @@ app.get('/api/posts/:year/:month', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
 
-    if (!req.body.name || req.body.name.length < 3) {
-        // 400 Bad Request 
-        res.status(400).send("Name is required and should be a minimum of 3 characters I'm afraid!")
-        return; 
-        
+    // if (!req.body.name || req.body.name.length < 3) {
+    //     // 400 Bad Request 
+    //     res.status(400).send("Name is required and should be a minimum of 3 characters I'm afraid!")
+    //     return; 
+
+    // }
+    // const course = {
+    //     id: courses.length + 1, 
+    //     name: req.body.name
+    // }
+
+    const schema = Joi.object({
+        name: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required()
+    })
+
+    const result = schema.validate(req.body)
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message)
+        return
+    } else {
+        courses.push(course)
+        res.send(course)
+        console.log("All systems go")
     }
-    const course = {
-        id: courses.length + 1, 
-        name: req.body.name
+})
+
+app.put('/api/courses/:id', (req, res) => {
+    //Look up the course with it's id; if it doesn't exist, we need to return 404
+    const course = courses.find((c) => c.id === parseInt(req.params.id))
+
+    if (!course) {
+        res.status(404).send('The course you are looking to overrite does not exist')
+    } else {
+        console.log("Hit me!")
     }
-    courses.push(course)
+
+    // If the course you wish to update exists, we need to validate that the course you are looking to replace the known course with
+    // has been structured appropriately 
+    // If invalid, return 400 - bad request 
+    const schema = Joi.object({
+
+        name: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required()
+    })
+
+    const result = schema.validate(req.body)
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message)
+        return
+    } else {
+        console.log('Feeling even better than I was the last time!')
+    }
+
+    // Update the course
+    // Return the updated course 
+    course.name = req.body.name 
     res.send(course)
 })
 
